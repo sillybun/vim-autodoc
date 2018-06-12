c4b3815b57e9560ecb666177092fbaaac6bb4cad
sillybun <zhangytfudan@gmail.com>
2018-06-12 20:18:01 +0800

    bug fix

--- ftplugin/python/gcd.py
+++ ftplugin/python/gcd.py
@@ -1,8 +1,16 @@
 def main() -> None:
+    """
+    called number: 1
+    total time: 0.00019097328186035156s
+    """
     print(gcd(15, 10))
     print(gcd(45, 12))
 
 def gcd(a: int, b: int) -> int:
+    """
+    called number: 2
+    total time: 4.0531158447265625e-06s
+    """
     while b:
         a, b = b, a%b
     return a
